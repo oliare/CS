2bfd02d5cd05fcfd7ed67e4ba3ef4f2b726b69b3
Olia <oliaopanasiukk@gmail.com>
2024-01-17 20:23:11 +0200

    intro to generics

---
+++ hw(24_26_generic_collections)/hw(24_26_generic_collections)/Employee.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace hw_24_26_generic_collections_
+{
+    internal class Employee
+    {
+        private string fullName;
+        private string position;
+        private double salary;
+        private string email;
+        public string Email
+        {
+            get => email;
+            set => email = string.IsNullOrEmpty(value)
+                || !value.Contains('@') ? "no email" : value;
+        }
+        public string Name
+        {
+            get => fullName;
+            set => fullName = string.IsNullOrEmpty(value) ? "no name" : value;
+        }
+        public string Position
+        {
+            get => position;
+            set => position = string.IsNullOrEmpty(value) ? "no position" : value;
+        }
+        public double Salary
+        {
+            get => salary;
+            set => salary = value > 0 ? value : 0;
+        }
+        public Employee(string name, string position,
+            double salary, string email)
+        {
+            Name = name;
+            Position = position;
+            Salary = salary;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return $"\tFull name: {fullName}\n\tPosition : {position}" +
+                $"\n\tSalary: {salary}$ \n\te-mail: {email}";
+        }
+    }
+    class Methods
+    {
+        List<Employee> employees = new List<Employee>();
+        public Methods(List<Employee> emp)
+        {
+            employees = emp;
+        }
+        public void AddEmp(Employee employee)
+        {
+            employees.Add(employee);
+            Console.WriteLine("* employee has been added");
+        }
+        public void RemoveEmp(string name)
+        {
+            var toRemove = employees.FirstOrDefault(x => x.Name == name);
+            if (toRemove != null)
+            {
+                employees.Remove(toRemove);
+                Console.WriteLine("* employee was removed >> " + name);
+            }
+            else
+            {
+                Console.WriteLine("! something went wrong");
+            }
+        }
+        public void ChangeEmp(Employee e, string name, string pos,
+            double salary, string email)
+        {
+            if (employees.IndexOf(e) != -1)
+            {
+                e.Name = name;
+                e.Position = pos;
+                e.Salary = salary;
+                e.Email = email;
+                Console.WriteLine($"* information about the employee has been changed >>\n" +
+                    $"Full name: {name}\nPosition : {pos}\nSalary: {salary}$\ne-mail: {email}");
+            }
+            else { Console.WriteLine("! failed to change information"); }
+        }
+        public void SearchByName(string name)
+        {            
+            if (employees.Any(x => x.Name == name))
+            {
+                Console.WriteLine("* employee found >> " + name);
+            }
+            else
+            {
+                Console.WriteLine($"{name} not found");
+            }
+        }
+        public void SortBySalary()
+        {
+            employees.Sort((e1, e2) => e1.Salary.CompareTo(e2.Salary));
+            Console.WriteLine("* employees are sorted by salary");
+        }
+    }
+
+}
---
+++ hw(24_26_generic_collections)/hw(24_26_generic_collections)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace hw_24_26_generic_collections_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            Employee e1 = new Employee("Marchuk Denis Viktorovych", "data scientist", 4800, "den@gmail.com");
+            Employee e2 = new Employee("Paray Dmytro Serhiyovych", "software engineer", 3100, "parayd@gmail.com");
+            Employee e3 = new Employee("Bondar Roman Volodymyrovych", "UI/UX designer", 2700, "bonrom@gmail.com");
+
+            Methods m = new Methods(list);
+            m.AddEmp(e1);
+            m.AddEmp(e2);
+            m.AddEmp(e3);
+            Print(list, "\t>>>>>>> DATA BASE <<<<<<<");
+            
+            m.SortBySalary();
+            Print(list, "\t>>>>>>> DATA BASE <<<<<<<");
+            
+            m.ChangeEmp(e3, "Bondarenko Vitaliy Viktorovych", "c# developer", 3500, "vitaliy@gmail.com");
+            Print(list, "\t>>>>>>> DATA BASE <<<<<<<");
+            
+            m.SearchByName("Paray Dmytro Serhiyovych");
+
+            m.RemoveEmp("Marchuk Denis Viktorovych");
+            Print(list, "\t>>>>>>> DATA BASE <<<<<<<");
+            
+            Console.WriteLine();
+        }
+        static void Print<T>(IEnumerable<T> list, string prompt = "")
+        {
+            Console.WriteLine("\n" + prompt + "\n");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+                Console.WriteLine("\t------------------------");
+            }
+            Console.WriteLine();
+
+        }
+
+    }
+}
